d595c2129e70c1aec94244c38487d4af4af59237
HaxiDenti <aldie.nightstar@gmail.com>
2018-12-15 18:55:44 +0200

    Improved new version

--- src/main/java/haxidenti/duckLib/Duck.java
+++ src/main/java/haxidenti/duckLib/Duck.java
@@ -1,11 +1,20 @@
 package haxidenti.duckLib;
 
+import java.lang.annotation.*;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 public class Duck {
-    public static <T> T quack(Class<T> clazz, Object o) {
+
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.RUNTIME)
+    public static @interface Rename {
+        String value();
+    }
+
+
+    public static <T> T quack(Class<T> duckClass, Object o) {
         try {
             if (Proxy.isProxyClass(o.getClass())) {
                 var handler = Proxy.getInvocationHandler(o);
@@ -13,7 +22,7 @@ public class Duck {
                     o = ((Handler) handler).getObjectInside();
                 }
             }
-            return (T) Proxy.newProxyInstance(o.getClass().getClassLoader(), new Class[]{clazz}, new Handler(o));
+            return (T) Proxy.newProxyInstance(o.getClass().getClassLoader(), new Class[]{duckClass}, new Handler(o, duckClass));
         } catch (Exception e) {
             e.printStackTrace();
             return null;
@@ -23,17 +32,31 @@ public class Duck {
     private static class Handler implements InvocationHandler {
         private Object object;
         private Class clazz;
+        private Class duckClass;
 
-        public Handler(Object object) {
+        public Handler(Object object, Class<?> duckClass) {
             this.object = object;
             this.clazz = object.getClass();
+            this.duckClass = duckClass;
         }
 
-        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        public Object invoke(Object proxy, Method callableMethod, Object[] args) throws Throwable {
             try {
-                if (method.getName().equals("data")) return object;
-                Method m = clazz.getDeclaredMethod(method.getName(), method.getParameterTypes());
-                return m.invoke(object, args);
+                String methodName = callableMethod.getName();
+                if (methodName.equals("data") && callableMethod.getParameterTypes().length < 1) {
+                    return object;
+                }
+
+                Method duckMethod = duckClass.getDeclaredMethod(methodName, callableMethod.getParameterTypes());
+
+                // Rename it, if there is an annotation to do that
+                Annotation renameDuckMethodAnnotation = duckMethod.getAnnotation(Rename.class);
+                if (renameDuckMethodAnnotation != null) {
+                    methodName = ((Rename) renameDuckMethodAnnotation).value();
+                }
+
+                Method proxyMethod = clazz.getDeclaredMethod(methodName, callableMethod.getParameterTypes());
+                return proxyMethod.invoke(object, args);
             } catch (Exception e) {
                 e.printStackTrace();
             }
